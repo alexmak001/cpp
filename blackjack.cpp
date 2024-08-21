47d7a0ac8ae95497f805db6f6fba0c9c2e6e68a6
alexmak001 <alexm7701@gmail.com>
2024-08-20 20:00:19 -0700

    game v1

--- blackjack.cpp
+++ blackjack.cpp
@@ -61,7 +61,7 @@ static map<Rank, std::string> rankToString = {
         };
 
 // map for values to work on enumerated variables
-static const std::map<Rank, int> blackjackValues = {
+static std::map<Rank, int> blackjackValues = {
         {Rank::Two, 2},
         {Rank::Three, 3},
         {Rank::Four, 4},
@@ -92,7 +92,7 @@ public:
     Card(Suit s, Rank r): enumSuit(s), enumRank(r){
         // initialize a card with suit and rank
 
-        cout << "Enum Card Created; " << "\t suit: " << suitToString[enumSuit] << "\t rank: " << rankToString[enumRank] << "\t value: " << blackjackValues[enumRank] << endl;
+        // cout << "Enum Card Created; " << "\t suit: " << suitToString[enumSuit] << "\t rank: " << rankToString[enumRank] << "\t value: " << blackjackValues[enumRank] << endl;
 
     };
 
@@ -138,12 +138,24 @@ class Deck{
     vector<Card> deck;
 
     public:
-    // initialize deck of cards
+    // intializes an empty deck of cards
     Deck(){
-        cout << "Constructor called" << endl;
+        // cout << "Constructor called" << endl;
 
         // create a deck of cards
 
+
+    };
+
+    // use random number generator to shuffle
+    void shuffle_deck(){
+        random_device rd; // device
+        mt19937 g(rd()); // algorithm to shuffle
+        shuffle(deck.begin(), deck.end(), g);
+    }
+
+    // fills the deck
+    void fill_deck(){
         // loop through enum variables and use them instead of Strings
         // TODO: use the keys in the map defined above instead of an array
         Suit allSuitsEnum[] = {Hearts, Spades, Diamonds, Clubs};
@@ -169,6 +181,8 @@ class Deck{
                 deck.emplace_back(Card(allSuitsEnum[i], allRanksEnum[j]));
             }
         };
+
+        shuffle_deck();
     };
 
     // sets game variable
@@ -182,15 +196,8 @@ class Deck{
         cout << "The game is: " << Deck::GAME << endl;
     }
 
-    // use random number generator to shuffle
-    void shuffle_deck(){
-        random_device rd; // device
-        mt19937 g(rd()); // algorithm to shuffle
-        shuffle(deck.begin(), deck.end(), g);
-    }
-
     // draw a card
-    void draw_a_card(){
+    Card draw_a_card(){
         // get card value
         cout << "Drew a card from the current deck" << endl;
 
@@ -205,19 +212,99 @@ class Deck{
         // remove last card
         deck.pop_back();
         
+        return curCard;
     };
 
+    // adds a card to the deck
+    void add_a_card(Card card){
+        deck.emplace_back(card);
+    }
+
+    // gets total score from deck
+    int get_score(){
+        int score = 0;
+        for(Card curCard : deck){
+            // show the value
+            score += curCard.get_value();
+        };
+
+        return score;
+    };
+
+    // prints deck
+    void print_deck(){
+        for(Card curCard : deck){
+            // show the value
+            cout << "\n  Card:" << endl;
+            curCard.get_value();
+            curCard.get_rank();
+            curCard.get_suit();
+        };
+        int score = get_score();
+        cout << "\nScore: " << score << endl;
+    };
+
+
+
 };
+
+// returns true if score is 21
+bool win_blackjack_game(Deck deck){
+    int score = deck.get_score();
+    if(score == 21){
+        return true;
+    }else{
+        return false;
+    };
+};
+
+// returns true if score is over 21
+bool over_21(Deck deck){
+    int score = deck.get_score();
+    if(score > 21){
+        return true;
+    }else{
+        return false;
+    };
+};
+
+bool draw_card_choice(){
+    char userInput;
+    do {
+            cout << "Please enter 'y' or 'n': ";
+            cin >> userInput;
+        
+            // Convert to lowercase to accept both 'Y' and 'y'
+            userInput = tolower(userInput);
+
+            // Check if the input is valid
+            if (userInput != 'y' && userInput != 'n') {
+                cout << "Invalid input. ";
+        }
+    } while (userInput != 'y' && userInput != 'n');
+
+    if(userInput == 'y'){
+        return true;
+    }else{
+        return false;
+    }
+}
+
 
 // TODO: make a fuction to run a blackjack game, maybe look into Player class (to hold Deck)
 // TODO: make deck class empty on intialization and create function to populat and shuffle it
 // TODO: make a add_card_to_deck() function
 
+// TODO:
+// create a status for player 0 active or out - if score over 21 or done drawing
+// while (status for each player){for player in each one to make sure their status is good}
 
 // Define the static member variable outside the class
 string Deck::GAME = "";
 
 int main() {
+    /*
+    // Testing Functionalities
     cout << "Hello World!" << endl;
 
     // testing Deck
@@ -266,6 +353,95 @@ int main() {
     Card enumCard = Card(enumSuit, enumRank);
 
     // testing value map
+    */
+
+    cout << "\n Playing a game of blackjack: \n";
+
+    // create dealer and populate
+    Deck dealer = Deck();
+    dealer.fill_deck();
+    dealer.print_deck();
+
+    // create players 
+    Deck player1 = Deck();
+    Deck player2 = Deck();
+
+    // each player draws two cards
+    player1.add_a_card(dealer.draw_a_card());
+    player2.add_a_card(dealer.draw_a_card());
+    player1.add_a_card(dealer.draw_a_card());
+    player2.add_a_card(dealer.draw_a_card());
+
+    // show each deck
+    cout << "\n Player 1 Deck: \n";
+    player1.print_deck();
+    cout << "\n Player 2 Deck: \n";
+    player2.print_deck();
+
+    // holds all players
+    vector<Deck> players = {player1, player2};
+
+    // status for players
+    bool player1Win = false;
+    bool player1Bust = false;
+
+    bool player2Win = false;
+    bool player2Bust = false;
+
+    bool endGame = false;
+
+    // bool draw1;
+    // bool draw2;
+
+    // playing the game
+    while(!endGame){
+
+        // give player 1 option to draw card
+        cout << "Player 1: Draw a card?" << endl;
+        bool draw1 = draw_card_choice();
+
+        if(draw1){
+            player1.add_a_card(dealer.draw_a_card());
+        }
+
+        // give player 2 option to draw card
+        cout << "Player 2: Draw a card?" << endl;
+        bool draw2 = draw_card_choice();
+
+        if(draw2){
+            player2.add_a_card(dealer.draw_a_card());
+        }
+
+        // check if player won
+        // check if player is over 21
+
+
+        player1Win = win_blackjack_game(player1);
+        if(player1Win){
+            cout << "Player 1 Won!" << endl;
+            endGame = true;
+        }
+
+        player1Bust = over_21(player1);
+        if(player1Bust){
+            cout << "Player 1 Bust!" << endl;
+            endGame = true;
+        }
+
+        player2Win = win_blackjack_game(player2);
+        if(player2Win){
+            cout << "Player 2 Won!" << endl;
+            endGame = true;
+        }
+
+        player2Bust = over_21(player2);        
+        if(player2Bust){
+            cout << "Player 2 Bust!" << endl;
+            endGame = true;
+        }
+
+    };
+    
 
     return 0;
 }
